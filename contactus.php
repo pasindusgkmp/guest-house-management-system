65b347cfb98054401979be43ab05e864e9bbd0b3
Pasidu Sathsara <pasindugunasekara889@gmail.com>
2024-06-06 11:04:27 +0530

    compleate contact us page

---
+++ contactus.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WORLDSFRIENDS-CONTACT</title>
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <?php require('inc/links.php'); ?>
+ 
+</head>
+<body class="bg-light">
+
+  <?php require('inc/header.php');   ?>
+
+  <div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">CONTACT US</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">Lorem ipsum dolor sit, amet consectetur
+       adipisicing elit. Perspiciatis, veniam quam. 
+       Nulla magni sit architecto voluptas, officiis voluptates
+        praesentium libero.<br>
+        Nulla magni sit architecto voluptas, officiis voluptates
+        praesentium libero.
+    </p>
+    
+  </div>
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 mb-5 px-4"> 
+
+        <div class="bg-white rounded shadow p-4">
+        <iframe class="w-100 rounded mb-4" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2612757.900106354!2d81.11686071478512!3d7.455763445745867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae451006e11ff1f%3A0x214a64c6087a5f31!2zTW9uYXJhZ2FsYSx0b3duLiDgtrjgt5zgtqvgtrvgt4_gtpzgtr0g4Lax4Lac4La74La6Lg!5e0!3m2!1sen!2slk!4v1716872804984!5m2!1sen!2slk" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <h5>Address</h5>
+          <a href="https://maps.app.goo.gl/bCsD5mTjcube1hwG9" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
+            <i class="bi bi-geo-alt-fill"></i> V8CW+289, Tissa Rd, Monaragala 
+          </a>
+          <h5 class="mt-4">Call Us</h5>
+        <a href="tel: +94703179882" class="d-inline-block mb-2 text-decoration-none text-dark">
+          <i class="bi bi-telephone-fill"></i> +94703179882
+        </a>
+        <br>
+        <a href="tel: +94703179882" class="d-inline-block text-decoration-none text-dark">
+          <i class="bi bi-telephone-fill"></i> +94703179882
+        </a>
+        <h5 class="mt-4">E-mail</h5>
+        <a href="mailto: ask.tjwebdev@gmail.com" class="d-inline-block text-decoration-none text-dark">
+          <i class="bi bi-envelope-at-fill"></i> ask.tjwebdev@gmail.com
+        </a>
+        <h5 class="mt-4">Follow Us</h5>
+        <a href="#" class="d-inline-block text-dark fs-5 me-2">
+          <i class="bi bi-twitter" me-1></i> 
+        </a>
+        
+        <a href="#" class="d-inline-block text-dark fs-5 me-2">
+          
+            <i class="bi bi-facebook" me-1></i> 
+         
+        </a>
+       
+        <a href="#" class="d-inline-block text-dark fs-5">
+          
+            <i class="bi bi-instagram" me-1></i> 
+          
+        </a>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6  px-4">
+        <div class="bg-white rounded shadow p-4">
+          <form>
+            <h5>Send a message</h5>
+            <div class="mt-3">
+              <label class="form-label" style="font-weight:500;">Name</label>
+              <input type="text" class="form-control shadow-none">
+            </div>
+            <div class="mt-3">
+              <label class="form-label" style="font-weight:500;">Email</label>
+              <input type="email" class="form-control shadow-none">
+            </div>
+            <div class="mt-3">
+              <label class="form-label" style="font-weight:500;">Subject</label>
+              <input type="text" class="form-control shadow-none">
+            </div>
+            <div class="mt-3">
+              <label class="form-label" style="font-weight:500;">Message</label>
+              <textarea class="form-control shadow-none" rows="12" style="resize: none;"></textarea>
+            </div>
+            <button type="submit" class="btn text-white custom-bg mt-3">SEND</button>
+          </form>
+        </div>
+      </div>
+
+      
+    </div>
+  </div>
+
+
+  <?php require('inc/footer.php');  ?>
+  
+    
+</body>
+</html>
+
+
