abcdb1c98ec34464c4c1bb5bc7618092bb6b2983
Pasidu Sathsara <pasindugunasekara889@gmail.com>
2024-06-07 12:16:41 +0530

    roos page complete and admin panel done

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guest house management system</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>WORLDSFRIENDS-HOME</title>
+    <?php require('inc/links.php'); ?>
     <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="css/common.css">
+    
     <style>
         
         .availability-form{
@@ -26,172 +24,8 @@
     </style>
 </head>
 <body class="bg-light">
-<nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
-  <div class="container-fluid">
-    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Friends World</a>
-    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link me-2" href="#">Rooms</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link me-2" href="#">Facilities</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link me-2" href="#">Contact Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-      </ul>
-      <div class="d-flex">
-        <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-         Login
-        </button>
 
-        <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
-         Register
-        </button>
-
-      </div>
-    </div>
-  </div>
-</nav>
-
-
-<div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form>
-      <div class="modal-header">
-        <h5 class="modal-title d-flex align-items-center">
-        <i class="bi bi-person-circle fs-3 me-2"></i> User Login
-        </h5>
-        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-        <div class="modal-body">
-          <div class="mb-3">
-              <label class="form-label">Email address</label>
-              <input type="email" class="form-control shadow-none">
-          </div>
-
-          <div class="mb-4">
-              <label class="form-label">Password</label>
-              <input type="password" class="form-control shadow-none">
-          </div>
-
-          <div class="d-flex align-items-center justify-content-between mb-2">
-            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-            <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password</a>
-          </div>
-      </div>
-      
-
-      </form>
-
-
-     
-    </div>
-  </div>
-</div>
-
-<div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <form>
-      <div class="modal-header">
-        <h5 class="modal-title d-flex align-items-center">
-        <i class="bi bi-person-lines-fill"></i>  User Registration
-        </h5>
-        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-        <div class="modal-body">
-
-          <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
-            Note: Your details must match with your ID(passport,driving licence,etc.)
-            that will be required during check-in.
-          </span>
-
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-md-6 ps-0 mb-3">
-                <label class="form-label">Name</label>
-                <input type="text" class="form-control shadow-none">
-
-              </div>
-
-              <div class="col-md-6 p-0 mb-3">
-                <label class="form-label">Email</label>
-                <input type="email" class="form-control shadow-none">
-                
-              </div>
-
-
-              <div class="col-md-6 ps-0 mb-3">
-                <label class="form-label">Phone Number</label>
-                <input type="number" class="form-control shadow-none">
-
-              </div>
-
-              <div class="col-md-6 p-0 mb-3">
-                <label class="form-label">Picture</label>
-                <input type="file" class="form-control shadow-none">
-                
-              </div>
-
-              <div class="col-md-12 p-0 mb-3">
-                <label class="form-label">Address</label>
-                <textarea class="form-control shadow-none" rows="1"></textarea>
-                
-              </div>
-
-              <div class="col-md-6 ps-0 mb-3">
-                <label class="form-label">Pin code</label>
-                <input type="number" class="form-control shadow-none">
-
-              </div>
-
-              <div class="col-md-6 p-0 mb-3">
-                <label class="form-label">Date of Birth</label>
-                <input type="date" class="form-control shadow-none">
-                
-              </div>
-
-              <div class="col-md-6 ps-0 mb-3">
-                <label class="form-label">Password</label>
-                <input type="password" class="form-control shadow-none">
-
-              </div>
-
-              <div class="col-md-6 p-0 mb-3">
-                <label class="form-label">Confirm Password</label>
-                <input type="password" class="form-control shadow-none">
-                
-              </div>
-
-              
-            </div>
-          </div>
-
-          <div class="text-center my-1">
-            <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
-        </div>
-      </div>
-      
-
-      </form>
-
-
-     
-    </div>
-  </div>
-</div>
+  <?php require('inc/header.php');   ?>
 
 <!--Carousel-->
 
@@ -233,12 +67,12 @@
       <form>
         <div class="row align-items-end">
           <div class="col-lg-3 mb-3">
-            <label class="form-label" style="font-weight:500;">Ckeck-In</label>
+            <label class="form-label" style="font-weight:500;">Check-In</label>
             <input type="date" class="form-control shadow-none">
           </div>
 
           <div class="col-lg-3 mb-3">
-            <label class="form-label" style="font-weight:500;">Ckeck-Out</label>
+            <label class="form-label" style="font-weight:500;">Check-Out</label>
             <input type="date" class="form-control shadow-none">
           </div>
 
@@ -324,6 +158,17 @@
               Room Heater
             </span>
           </div>
+          
+          <div class="guests mb-4">
+            <h6 class="mb-1">Guests</h6>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              5 Adults
+            </span>
+
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              4 Children
+            </span>
+          </div>
 
           <div class="rating mb-4">
             <h6 class="mb-1">Rating</h6>
@@ -395,6 +240,17 @@
             </span>
           </div>
 
+          <div class="guests mb-4">
+            <h6 class="mb-1">Guests</h6>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              5 Adults
+            </span>
+
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              4 Children
+            </span>
+          </div>
+
           <div class="rating mb-4">
             <h6 class="mb-1">Rating</h6>
             <span class="badge rounded-pill bg-light">
@@ -447,6 +303,7 @@
           </div>
 
           <div class="facilities mb-4">
+
             <h6 class="mb-1">facilities</h6>
             <span class="badge rounded-pill bg-light text-dark text-wrap">
               wifi
@@ -462,6 +319,17 @@
 
             <span class="badge rounded-pill bg-light text-dark text-wrap">
               Room Heater
+            </span>
+          </div>
+
+          <div class="guests mb-4">
+            <h6 class="mb-1">Guests</h6>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              5 Adults
+            </span>
+
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+              4 Children
             </span>
           </div>
 
@@ -610,11 +478,12 @@
 <!--Reach Us-->
 <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
 
-<div class="conteiner">
-  <div class="row">
-    <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+<div class="conteiner ">
+  <div class="row ">
+    <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded"     >
       <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2612757.900106354!2d81.11686071478512!3d7.455763445745867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae451006e11ff1f%3A0x214a64c6087a5f31!2zTW9uYXJhZ2FsYSx0b3duLiDgtrjgt5zgtqvgtrvgt4_gtpzgtr0g4Lax4Lac4La74La6Lg!5e0!3m2!1sen!2slk!4v1716872804984!5m2!1sen!2slk" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
+
     <div class="col-lg-4 col-md-4">
       <div class="bg-white p-4 rounded mb-4">
         <h5>Call Us</h5>
@@ -655,56 +524,11 @@
 
 
 <!--Footer-->
-<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Footer</h2>
-
-<div class="container-fluid bg-white mt-5">
-  <div class="row">
-    <div class="col-lg-4 p-4">
-      <h3 class="h-font fw-bold fs-3 mb-2">Friends Worlds</h3>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Natus dolore velit numquam amet quidem beatae ipsum incidunt 
-        reprehenderit odit illo! Soluta obcaecati ad tempore quibusdam
-        officiis cupiditate atque sunt blanditiis.
-      </p>
-    </div>
-    <div class="col-lg-4 p-4">
-      <h5 class="mb-3">Links</h5>
-      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
-      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
-      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
-      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
-      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
-    </div>
-    <div class="col-lg-4 p-4">
-      <h5 class="mb-3">Follow Us</h5>
-      <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
-      
-          <i class="bi bi-twitter" me-1></i> Twitter
-        
-      </a><br>
-
-      <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
-      
-        <i class="bi bi-facebook" me-1></i> Facebook
-      
-    </a><br>
-
-    <a href="#" class="d-inline-block text-dark text-decoration-none ">
-      
-      <i class="bi bi-instagram" me-1></i> Instagram
-    
-  </a><br>
-
-    </div>
-  </div>
-  
-</div>
-<h6 class="text-center bg-white text-white p-3 m-0">Design and Developed By Pasindu Sathsara</h6>
+<?php require('inc/footer.php');  ?>
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
 
